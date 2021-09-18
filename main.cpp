1207454324e8f7229ff2f739c3e02b0307bb790a
Manuel Pepe <manuel.pepe@stud.unifi.it>
2021-09-18 11:48:19 +0200

    main cleaned

--- main.cpp
+++ main.cpp
@@ -1,29 +1,27 @@
 #include <QApplication>
-#include <QPushButton>
 #include <QHBoxLayout>
+#include <QFile>
+
 #include <iostream>
 #include "loginform.h"
-#include "mainwindow.h"
 #include "calendar.h"
-#include <QFile>
+
 
 int main(int argc, char *argv[]) {
     QApplication a(argc, argv);
 
     //Set the app style sheet
     QFile styleSheetStyle(":/QTheme/Fibers.qss");
-    if(styleSheetStyle.exists()){
-        std::cout<<"esiste"<<std::endl;
+    if (styleSheetStyle.exists()) {
+        bool flag = styleSheetStyle.open(QFile::ReadOnly | QFile::Text);
+        if (flag) {
+            QString styleSheet = QLatin1String(styleSheetStyle.readAll());
+            a.setStyleSheet(styleSheet);
+        }
+    } else {
+        std::cerr << "Could not open stylesheet file\n";
     }
-    bool flag= styleSheetStyle.open(QFile::ReadOnly | QFile::Text);
-    if(flag){
-        std::cout<<"file aperto per la lettura"<<std::endl;
-    }
-    QString styleSheet=QLatin1String(styleSheetStyle.readAll());
-    a.setStyleSheet(styleSheet);
 
-
-    //MainWindow mainWindow;
     ConnectionManager connectionManager;
     LoginForm loginForm(nullptr, &connectionManager);
     Calendar calendar(nullptr, &connectionManager);
