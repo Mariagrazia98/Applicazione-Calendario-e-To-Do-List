df9a0ae67eae04dc74f7264a9d447e4119a0d38a
_mary <mary_pal@live.it>
2021-09-18 09:01:51 +0200

    Set a GUI Theme

--- main.cpp
+++ main.cpp
@@ -5,9 +5,23 @@
 #include "loginform.h"
 #include "mainwindow.h"
 #include "calendar.h"
+#include <QFile>
 
 int main(int argc, char *argv[]) {
     QApplication a(argc, argv);
+
+    //Set the app style sheet
+    QFile styleSheetStyle(":/QTheme/Fibers.qss");
+    if(styleSheetStyle.exists()){
+        std::cout<<"esiste"<<std::endl;
+    }
+    bool flag= styleSheetStyle.open(QFile::ReadOnly | QFile::Text);
+    if(flag){
+        std::cout<<"file aperto per la lettura"<<std::endl;
+    }
+    QString styleSheet=QLatin1String(styleSheetStyle.readAll());
+    a.setStyleSheet(styleSheet);
+
 
     //MainWindow mainWindow;
     ConnectionManager connectionManager;
